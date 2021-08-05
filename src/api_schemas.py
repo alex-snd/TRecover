a4cef07ac4171f999805a2bdb9d556a35e8275ee
alex-snd <alexandershulga.sh@gmail.com>
2021-08-05 09:31:06 +0300

    Add noise range inspection

--- src/api_schemas.py
+++ src/api_schemas.py
@@ -9,29 +9,36 @@ class PredictPayload(BaseModel):
     beam_width: int
     delimiter: str = ''
 
-    @classmethod
     @validator('data')
-    def data_validator(cls, value: str) -> str or None:
-        if not value:
+    def data_validator(cls, data: str) -> str or None:
+        if not data:
             ValueError(f'Data for zread must contain at least one character')
 
-        return value
+        return data
 
-    @classmethod
-    @validator('min_noise', 'max_noise')
-    def noise_validator(cls, value: int) -> int or None:
-        if not 0 <= value <= len(Collate.num_to_alphabet):
-            raise ValueError(f'Noise range must be in between 0 and {len(Collate.num_to_alphabet)}')
+    @validator('min_noise')
+    def min_noise_validator(cls, min_noise: int) -> int or None:
+        if not 0 <= min_noise <= len(Collate.num_to_alphabet):
+            raise ValueError(f'Minimum noise range must be in between 0 and {len(Collate.num_to_alphabet)}')
 
-        return value
+        return min_noise
 
-    @classmethod
-    @validator('min_noise', 'max_noise')
-    def beam_width_validator(cls, value: int) -> int or None:
-        if not 1 <= value <= len(Collate.num_to_alphabet):
+    @validator('max_noise')
+    def max_noise_validator(cls, max_noise: int, values: dict) -> int or None:
+        if not 0 <= max_noise <= len(Collate.num_to_alphabet):
+            raise ValueError(f'Maximum noise range must be in between 0 and {len(Collate.num_to_alphabet)}')
+
+        if values['min_noise'] > max_noise:
+            raise ValueError('Maximum noise range must be grater than minimum noise range')
+
+        return max_noise
+
+    @validator('beam_width')
+    def beam_width_validator(cls, beam_width: int) -> int or None:
+        if not 1 <= beam_width <= len(Collate.num_to_alphabet):
             raise ValueError(f'Beam width must be in between 1 and {len(Collate.num_to_alphabet)}')
 
-        return value
+        return beam_width
 
     class Config:
         """ Needed to declare examples of data the app can receive for API documentation"""
