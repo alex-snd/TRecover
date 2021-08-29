fec377cfcddef1ce1749141cd06d5b4313933302
alex-snd <alexandershulga.sh@gmail.com>
2021-08-29 12:51:48 +0300

    Add optimizer_to_str function

--- src/utils.py
+++ src/utils.py
@@ -11,6 +11,7 @@ import torch
 import torch.nn.functional as F
 import typer
 from torch import Tensor
+from torch.optim import Optimizer
 
 import config
 from data import Collate
@@ -124,6 +125,15 @@ def set_seeds(seed: int = 2531) -> None:
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)  # multi-GPU
+
+
+def optimizer_to_str(optimizer: Optimizer) -> str:
+    optimizer_str_repr = str(optimizer)
+
+    optimizer_name = optimizer_str_repr.split()[0]
+    optimizer_params = re.findall(r'(.+): (.+)', optimizer_str_repr)
+
+    return f"{optimizer_name}({', '.join([f'{param.strip()}={value.strip()}' for param, value in optimizer_params])})"
 
 
 def get_model(token_size: int,
