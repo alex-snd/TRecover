c69223bbbf06e0fba55e5d7c8a026cd0366cc838
alex-snd <alexandershulga.sh@gmail.com>
2021-09-01 15:28:02 +0300

    Implement colorful printing with rich. Remove console_width parameter.

--- src/zreadercli.py
+++ src/zreadercli.py
@@ -6,9 +6,13 @@ from zipfile import ZipFile
 
 import requests
 import torch
-import typer
+from rich.console import Group
+from rich.panel import Panel
+from rich.progress import Progress, TextColumn, BarColumn, DownloadColumn, TransferSpeedColumn
+from rich.text import Text
 from typer import Typer, Argument, Option
 
+import config
 import utils
 
 cli = Typer(name='ZreaderAPI')
@@ -35,7 +39,7 @@ def download_from_disk(sharing_link: str, save_dir: str) -> Optional[Path]:
     """
 
     if not (direct_link := utils.get_real_direct_link(sharing_link)):
-        typer.secho(f'Failed to download "{sharing_link}"', fg=typer.colors.BRIGHT_RED, bold=True)
+        config.project_logger.error(f'[red]Failed to download data from [/][bright_blue] {sharing_link}')
         return None
 
     filename = utils.extract_filename(direct_link) or 'downloaded_data'  # Try to recover the filename from the link
@@ -48,15 +52,22 @@ def download_from_disk(sharing_link: str, save_dir: str) -> Optional[Path]:
         if total_length is None:  # no content length header
             fw.write(response.content)
         else:
-            stream_iterator = response.iter_content(chunk_size=4096)
-            length = int(total_length) // 4096
-            label = typer.style('Downloading', fg=typer.colors.BLUE, bold=True)
+            data_stream = response.iter_content(chunk_size=4096)
 
-            with typer.progressbar(stream_iterator, length=length, label=label) as stream:
-                for data in stream:
+            with Progress(  # TODO add columns
+                    TextColumn('{task.description}', style='bright_blue'),
+                    BarColumn(complete_style='bright_blue'),
+                    DownloadColumn(),
+                    TransferSpeedColumn(),
+                    transient=True,
+            ) as progress:
+                download_progress = progress.add_task('Downloading', total=int(total_length))
+
+                for data in data_stream:
                     fw.write(data)
+                    progress.update(download_progress, advance=4096)
 
-    typer.secho(f'Downloaded "{filename}" to "{Path(save_dir, filename).absolute()}"', fg=typer.colors.GREEN, bold=True)
+    config.project_logger.info(f'[green]Downloaded "{filename}" to {Path(save_dir, filename).absolute()}')
 
     return filepath
 
@@ -116,18 +127,17 @@ def zread(inference_path: str = Argument(..., help='Path to file or dir for infe
           min_noise: int = Option(3, help='Min noise parameter. Minimum value is alphabet size'),
           max_noise: int = Option(5, help='Max noise parameter. Maximum value is alphabet size'),
           beam_width: int = Option(1, help='Width for beam search algorithm. Maximum value is alphabet size'),
-          console_width: int = Option(0, help='Console width for visualization. Zero value means for no restrictions'),
+          n_to_show: int = Option(0, help='Console width for visualization. Zero value means for no restrictions'),
           delimiter: str = Option('', help='Delimiter for columns visualization')
           ) -> None:
     inference_path = Path(inference_path)
 
     if not noisy and min_noise >= max_noise:
-        typer.secho('Maximum noise range must be grater than minimum noise range',
-                    fg=typer.colors.BRIGHT_RED, bold=True)
+        config.project_logger.error('[red]Maximum noise range must be grater than minimum noise range')
         return
 
     if not any([inference_path.is_file(), inference_path.is_dir()]):
-        typer.secho('Files for inference needed to be specified', fg=typer.colors.BRIGHT_RED, bold=True)
+        config.project_logger.error('[red]Files for inference needed to be specified')
         return
 
     artifacts = utils.load_artifacts(Path(model_artifacts))
@@ -137,29 +147,47 @@ def zread(inference_path: str = Argument(..., help='Path to file or dir for infe
                                device, weights=Path(weights_path))
     z_reader.eval()
 
-    files, files_columns = utils.get_files_columns(inference_path, separator, noisy, min_noise, max_noise,
-                                                   console_width, delimiter)
+    if z_reader.pe_max_len < n_to_show:
+        config.project_logger.error(f'[red]Parameter n_to_show={n_to_show} must be less than {z_reader.pe_max_len}')
+        return
+
+    elif n_to_show == 0:
+        n_to_show = z_reader.pe_max_len
+
+    files, files_columns = utils.get_files_columns(inference_path, separator, noisy, min_noise, max_noise, n_to_show)
     files_src = utils.files_columns_to_tensors(files_columns, device)
 
     for file_id, (file, src) in enumerate(zip(files, files_src), start=1):
         start_time = time()
 
-        loop_label = typer.style(f'[{file_id}/{len(files_src)}] Processing {file.name}',
-                                 fg=typer.colors.BLUE, bold=True)
+        loop_label = f'{file_id}/{len(files_src)} Processing {file.name}'
         chains = utils.beam_search(src, z_reader, beam_width, device,
                                    beam_loop=utils.cli_interactive_loop(label=loop_label))
+        chains = [Text(utils.visualize_target(chain, delimiter=delimiter), style='cyan', justify='center',
+                       overflow='ellipsis', end='\n\n') for (chain, _) in chains]
 
-        src_scale = src.size(0) * max(2 * len(delimiter), 1) + 1 * len(delimiter)
-        printing_scale = console_width if 0 < console_width < src_scale else src_scale
+        columns_as_rows = utils.visualize_columns(src, delimiter=delimiter, as_rows=True)
+        columns = (Text(row, style='bright_blue', overflow='ellipsis', no_wrap=True) for row in columns_as_rows)
 
-        print('-' * printing_scale)
-        print(utils.visualize_columns(src, delimiter=delimiter))
-        print('-' * printing_scale, end='\n\n')
-        for chain, _ in chains:
-            print(utils.visualize_target(chain, delimiter=delimiter), end='\n\n')
+        panel_group = Group(
+            Text('Columns', style='magenta', justify='center'),
+            *columns,
+            Text('Predicted', style='magenta', justify='center'),
+            *chains
+        )
 
-        typer.secho(f'Elapsed: {time() - start_time:>7.3f}s\n', fg=typer.colors.BLUE, bold=True)
+        config.project_console.print(
+            Panel(panel_group, title=file.name, border_style='magenta'),
+            justify='center'
+        )
+
+        config.project_console.print(f'\nElapsed: {time() - start_time:>7.3f} s\n', style='bright_blue')
 
 
 if __name__ == '__main__':
-    cli()
+    try:
+        cli()
+    except Exception as e:
+        config.project_logger.error(e)
+        config.project_console.print_exception(show_locals=True)
+        config.error_console.print_exception(show_locals=True)
