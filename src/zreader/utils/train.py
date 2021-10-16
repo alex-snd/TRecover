9889dfe618a0711305b7af938c6f84c3d718eca1
alex-snd <alexandershulga.sh@gmail.com>
2021-10-16 15:10:38 +0300

    Add ExperimentParams class

--- src/zreader/utils/train.py
+++ src/zreader/utils/train.py
@@ -1,8 +1,24 @@
 import re
+from argparse import Namespace
+from typing import Any, Optional
 
 import numpy as np
 import torch
 from torch.optim import Optimizer
+
+
+class ExperimentParams(dict):
+    def __init__(self, params: Optional[Namespace] = None):
+        super(ExperimentParams, self).__init__()
+
+        if params:
+            self.__dict__.update(vars(params))
+
+    def __getitem__(self, key: Any) -> Any:
+        return self.__dict__[key]
+
+    def update(self, *args: Any, **kwargs: Any) -> None:
+        self.__dict__.update(*args, **kwargs)
 
 
 def set_seeds(seed: int = 2531) -> None:
