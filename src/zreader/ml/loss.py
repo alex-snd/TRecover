41724a041b33713d810cbb04bc05760872619813
alex-snd <alexandershulga.sh@gmail.com>
2021-10-21 20:02:31 +0300

    Add src padding usage for loss function calculation

--- src/zreader/ml/loss.py
+++ src/zreader/ml/loss.py
@@ -4,10 +4,28 @@ from torch import Tensor
 from torch.nn import CrossEntropyLoss
 
 
-class CustomLoss(torch.nn.Module):
-    def __init__(self):
-        super(CustomLoss, self).__init__()
-        self.criterion = CrossEntropyLoss(ignore_index=-1)
+class CustomCrossEntropyLoss(torch.nn.Module):
+    def __init__(self, ignore_index: int = -1):
+        super(CustomCrossEntropyLoss, self).__init__()
 
-    def forward(self, tgt_out: Tensor, tgt: Tensor, src: Tensor) -> Tensor:
-        return self.criterion(tgt_out, tgt) + torch.mean(-1.0 * F.log_softmax(tgt_out, dim=-1) * ~src.bool())
+        self.criterion = CrossEntropyLoss(ignore_index=ignore_index)
+
+    def forward(self, *tensors) -> Tensor:
+        tgt, tgt_out = tensors[2], tensors[6]
+
+        return self.criterion(tgt_out, tgt)
+
+
+class CustomPenaltyLoss(torch.nn.Module):
+    def __init__(self, ignore_index: int = -1):
+        super(CustomPenaltyLoss, self).__init__()
+
+        self.criterion = CrossEntropyLoss(ignore_index=ignore_index)
+
+    def forward(self, *tensors) -> Tensor:
+        src, _, tgt, src_pad_mask, _, _, tgt_out = tensors
+
+        penalty_size = src_pad_mask.size(0) * src_pad_mask.size(1)
+        penalty_tensor = -1.0 * torch.flatten(F.log_softmax(tgt_out, dim=-1) * ~src.bool())[:penalty_size]
+
+        return self.criterion(tgt_out, tgt) + penalty_tensor.mean()
