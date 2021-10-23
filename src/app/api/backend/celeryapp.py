15e04ade91511336c7cb6fbfcfd8b1b8ebd6e0bb
alex-snd <alexandershulga.sh@gmail.com>
2021-10-23 17:51:03 +0300

    Add celery logging initialization (and remove it from config)

--- src/app/api/backend/celeryapp.py
+++ src/app/api/backend/celeryapp.py
@@ -1,20 +1,21 @@
 import celery
+from celery.signals import after_setup_task_logger, after_setup_logger
 
-import config
+from config import vars, log
 
 
 class Celery(celery.Celery):
 
     def gen_task_name(self, name: str, module: str) -> str:
-        if module.startswith('ml.'):
+        if module.startswith('src.'):
             module = module[4:]
 
         return super().gen_task_name(name, module)
 
 
 celery_app = Celery('ZReader',
-                    broker=config.CELERY_BROKER,
-                    backend=config.CELERY_BACKEND,
+                    broker=vars.CELERY_BROKER,
+                    backend=vars.CELERY_BACKEND,
                     include=['app.api.backend.tasks']
                     )
 
@@ -27,3 +28,16 @@ celery_app.conf.update({
     'task_track_started': True,
     'task_reject_on_worker_lost': True,
 })
+
+
+# Configure Celery logger
+@after_setup_task_logger.connect
+def setup_task_logger(logger, *args, **kwargs):
+    logger.addHandler(hdlr=log.error_handler)
+    logger.addHandler(hdlr=log.info_handler)
+
+
+@after_setup_logger.connect
+def setup_task_logger(logger, *args, **kwargs):
+    logger.addHandler(hdlr=log.error_handler)
+    logger.addHandler(hdlr=log.info_handler)
