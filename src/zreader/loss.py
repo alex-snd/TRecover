CustomPenaltyLoss calculation bug fix

src/zreader/loss.py
@@ -25,7 +25,7 @@ class CustomPenaltyLoss(torch.nn.Module):
     def forward(self, *tensors) -> Tensor:
         src, _, tgt, src_pad_mask, _, _, tgt_out = tensors
 
-        penalty_size = src_pad_mask.shape[0] * src_pad_mask.shape[1]
-        penalty_tensor = -1.0 * torch.flatten(F.log_softmax(tgt_out, dim=-1) * ~src.bool())[:penalty_size]
+        penalty_tensor = -1.0 * torch.masked_select(F.log_softmax(tgt_out, dim=-1) * ~src.bool(),
+                                                    mask=~src_pad_mask.view(-1, 1))
 
         return self.criterion(tgt_out, tgt) + penalty_tensor.mean()