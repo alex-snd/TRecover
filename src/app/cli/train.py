6ca8e4f1b5c86a8b6e3ba0ac19132efe73b316e9
alex-snd <alexandershulga.sh@gmail.com>
2021-11-21 16:40:18 +0300

    Add base train cli command

--- src/app/cli/train.py
+++ src/app/cli/train.py
@@ -1,8 +1,21 @@
-from typer import Typer
+from typer import Typer, Context, Option
 
 from config import log
 
 cli = Typer(name='Train-cli', add_completion=False)
+
+
+@cli.command(context_settings={"allow_extra_args": True, "ignore_unknown_options": True}, add_help_option=False)
+def local(ctx: Context, show_help: bool = Option(False, '--help', '-h', is_flag=True,
+                                                 help='Show help message and exit.')):
+    from zreader.train.base import get_parser, get_experiment_params, train
+
+    if show_help:
+        get_parser().print_help()
+        ctx.exit(0)
+
+    train(params=get_experiment_params(ctx.args))
+
 
 if __name__ == '__main__':
     try:
