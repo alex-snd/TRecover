adbcb614412da4bb0d833f919f5006f1d7e598de
alex-snd <alexandershulga.sh@gmail.com>
2021-12-04 11:56:47 +0300

    Add tensor_to_columns and tensor_to_target functions

--- src/zreader/utils/data.py
+++ src/zreader/utils/data.py
@@ -196,3 +196,14 @@ def files_columns_to_tensors(files_columns: List[List[str]],
                              device: torch.device = torch.device('cpu')
                              ) -> List[Tensor]:
     return [columns_to_tensor(columns, device) for columns in files_columns]
+
+
+def tensor_to_columns(grid: Tensor) -> List[str]:
+    return [
+        ''.join([var.NUM2ALPHABET[pos] for pos in range(grid.shape[1]) if grid[c, pos]])
+        for c in range(grid.shape[0])
+    ]
+
+
+def tensor_to_target(tgt: Tensor) -> List[str]:
+    return [var.NUM2ALPHABET[ch_id] for ch_id in tgt.tolist()]
