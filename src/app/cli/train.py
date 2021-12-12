a19b11b267e51a2b37411686d6f3b0cc369f9b8f
alex-snd <alexandershulga.sh@gmail.com>
2021-12-12 17:22:17 +0300

    Update cli interface

--- src/app/cli/train.py
+++ src/app/cli/train.py
@@ -8,31 +8,24 @@ cli = Typer(name='Train-cli', add_completion=False)
 @cli.command(context_settings={"allow_extra_args": True, "ignore_unknown_options": True}, add_help_option=False)
 def local(ctx: Context, show_help: bool = Option(False, '--help', '-h', is_flag=True,
                                                  help='Show help message and exit.')):
-    from zreader.train.base import get_parser, get_experiment_params, train
+    from zreader.train.local import train, get_parser
 
     if show_help:
         get_parser().print_help()
-        ctx.exit(0)
-
-    train(params=get_experiment_params(ctx.args))
+    else:
+        try:
+            train(args=ctx.args)
+        except Exception as ee:
+            log.project_logger.error(ee)
+            log.project_console.print_exception()
+            log.error_console.print_exception(show_locals=True)
 
 
 @cli.command(context_settings={"allow_extra_args": True, "ignore_unknown_options": True}, add_help_option=False)
-def wandb(ctx: Context, show_help: bool = Option(False, '--help', '-h', is_flag=True,
-                                                 help='Show help message and exit.')):
-    from zreader.train.wandb_train import get_parser, get_experiment_params, train
-
-    if show_help:
-        get_parser().print_help()
-        ctx.exit(0)
-
-    train(params=get_experiment_params(ctx.args))
+def remote(ctx: Context, show_help: bool = Option(False, '--help', '-h', is_flag=True,
+                                                  help='Show help message and exit.')):
+    pass
 
 
 if __name__ == '__main__':
-    try:
-        cli()
-    except Exception as e:
-        log.project_logger.error(e)
-        log.project_console.print_exception(show_locals=True)
-        log.error_console.print_exception(show_locals=True)
+    cli()
