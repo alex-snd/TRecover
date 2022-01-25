c48994e146baf8e233ad7fcce5f14ae93de1acd1
alex-snd <alexandershulga.sh@gmail.com>
2022-01-25 22:08:27 +0300

    Add documentation

--- src/app/cli/mlflow.py
+++ src/app/cli/mlflow.py
@@ -2,11 +2,22 @@ from typer import Typer, Option, Context, BadParameter
 
 from config import var, log
 
-cli = Typer(name='Mlflow-cli', add_completion=False)
+cli = Typer(name='Mlflow-cli', add_completion=False, help='Manage Mlflow service')
 
 
 @cli.callback(invoke_without_command=True)
 def mlflow_state_verification(ctx: Context) -> None:
+    """
+    Perform cli commands verification (state checking).
+
+    Parameters
+    ----------
+    ctx : Context
+        Typer (Click like) special internal object that holds state relevant
+        for the script execution at every single level.
+
+   """
+
     if var.MLFLOW_PID.exists():
         if ctx.invoked_subcommand in ('start', None):
             log.project_console.print(':rocket: The mlflow service is already started', style='bright_blue')
@@ -22,7 +33,7 @@ def mlflow_state_verification(ctx: Context) -> None:
         ctx.exit(1)
 
 
-@cli.command(name='start')
+@cli.command(name='start', help='Start service')
 def mlflow_start(host: str = Option(var.MLFLOW_HOST, '--host', '-h', help='Bind socket to this host.'),
                  port: int = Option(var.MLFLOW_PORT, '--port', '-p', help='Bind socket to this port.'),
                  concurrency: int = Option(var.MLFLOW_WORKERS, '-c',
@@ -33,6 +44,40 @@ def mlflow_start(host: str = Option(var.MLFLOW_HOST, '--host', '-h', help='Bind 
                  only_ui: bool = Option(False, '--only-ui', is_flag=True, help='Launch only the Mlflow tracking UI'),
                  attach: bool = Option(False, '--attach', '-a', is_flag=True, help='Attach output and error streams')
                  ) -> None:
+    """
+    Start dashboard service.
+
+    Parameters
+    ----------
+    host : str, default=ENV(MLFLOW_HOST) or 'localhost'
+        The address where the server will listen for client and browser connections.
+        Use this if you want to bind the server to a specific address. If set, the server
+        will only be accessible from this address, and not from any aliases (like localhost).
+
+    port : int, default=ENV(MLFLOW_PORT) or 8002
+        The port where the server will listen for browser connections.
+
+    concurrency : int, default=ENV(MLFLOW_WORKERS) or 1
+        The number of mlflow server workers.
+
+    registry : str, default=ENV(MLFLOW_REGISTRY_DIR) or 'file:///<BASE_DIR>/experiments/mlflow_registry'
+        URI to which to persist experiment and run data. Acceptable URIs are SQLAlchemy-compatible
+        database connection strings (e.g. 'sqlite:///path/to/file.db') or
+        local filesystem URIs (e.g. 'file:///absolute/path/to/directory').
+        By default, data will be logged to the ./mlruns directory.
+
+    backend_uri : str, default=ENV(MLFLOW_BACKEND) or 'sqlite:///<BASE_DIR>/experiments/mlflow_registry/mlflow.db'
+        Local or S3 URI to store artifacts, for new experiments. Note that this
+        flag does not impact already-created experiments.
+
+    only_ui : bool, default=False
+        Launch only the Mlflow tracking UI.
+
+    attach : bool, default=False
+        Attach output and error streams.
+
+    """
+
     import platform
     from zreader.utils.cli import start_service
 
@@ -58,24 +103,38 @@ def mlflow_start(host: str = Option(var.MLFLOW_HOST, '--host', '-h', help='Bind 
         mlflow_attach(live=False)
 
 
-@cli.command(name='stop')
+@cli.command(name='stop', help='Stop service')
 def mlflow_stop() -> None:
+    """ Stop dashboard service. """
+
     from zreader.utils.cli import stop_service
 
     stop_service(name='mlflow', pidfile=var.MLFLOW_PID)
 
 
-@cli.command(name='status')
+@cli.command(name='status', help='Display service status')
 def mlflow_status() -> None:
+    """ Display dashboard service status. """
+
     from zreader.utils.cli import check_service
 
     check_service(name='mlflow', pidfile=var.MLFLOW_PID)
 
 
-@cli.command(name='attach')
+@cli.command(name='attach', help='Attach local output stream to a service')
 def mlflow_attach(live: bool = Option(False, '--live', '-l', is_flag=True,
                                       help='Stream only fresh log records')
                   ) -> None:
+    """
+    Attach local output stream to a running dashboard service.
+
+    Parameters
+    ----------
+    live : bool, Default=False
+        Stream only fresh log records
+
+    """
+
     from zreader.utils.cli import stream
 
     with log.project_console.screen():
