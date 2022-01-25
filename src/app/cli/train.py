b1e5ec0d1ac1f765c76999a48f87336a76f7fc13
alex-snd <alexandershulga.sh@gmail.com>
2022-01-25 22:16:28 +0300

    Add documentation

--- src/app/cli/train.py
+++ src/app/cli/train.py
@@ -2,12 +2,28 @@ from typer import Typer, Context, Option
 
 from config import log
 
-cli = Typer(name='Train-cli', add_completion=False)
+cli = Typer(name='Train-cli', add_completion=False, help='Manage training')
 
 
-@cli.command(context_settings={"allow_extra_args": True, "ignore_unknown_options": True}, add_help_option=False)
-def local(ctx: Context, show_help: bool = Option(False, '--help', '-h', is_flag=True,
-                                                 help='Show help message and exit.')):
+@cli.command(context_settings={"allow_extra_args": True, "ignore_unknown_options": True},
+             add_help_option=False,
+             help='Start local training')
+def local(ctx: Context,
+          show_help: bool = Option(False, '--help', '-h', is_flag=True, help='Show help message and exit.')):
+    """
+    Start local training.
+
+    Parameters
+    ----------
+    ctx : Context
+        Typer (Click like) special internal object that holds state relevant
+        for the script execution at every single level.
+
+    show_help : bool, default=False
+        Show local train options.
+
+    """
+
     from zreader.train.local import train, get_parser
 
     if show_help:
@@ -15,15 +31,32 @@ def local(ctx: Context, show_help: bool = Option(False, '--help', '-h', is_flag=
     else:
         try:
             train(args=ctx.args)
-        except Exception as ee:
-            log.project_logger.error(ee)
+        except Exception as e:
+            log.project_logger.error(e)
             log.project_console.print_exception()
             log.error_console.print_exception(show_locals=True)
 
 
-@cli.command(context_settings={"allow_extra_args": True, "ignore_unknown_options": True}, add_help_option=False)
-def remote(ctx: Context, show_help: bool = Option(False, '--help', '-h', is_flag=True,
-                                                  help='Show help message and exit.')):
+@cli.command(context_settings={"allow_extra_args": True, "ignore_unknown_options": True},
+             add_help_option=False,
+             help='Start remote training')
+def remote(ctx: Context,
+           show_help: bool = Option(False, '--help', '-h', is_flag=True, help='Show help message and exit.')
+           ):
+    """
+    Start remote training.
+
+    Parameters
+    ----------
+    ctx : Context
+        Typer (Click like) special internal object that holds state relevant
+        for the script execution at every single level.
+
+    show_help : bool, default=False
+        Show remote train options.
+
+    """
+
     pass
 
 
