efadd28626e8f216597df5baf41666e21211ee0f
alex-snd <alexandershulga.sh@gmail.com>
2022-03-20 15:19:56 +0300

    Add dashboard file

---
+++ dashboard.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+
+def main() -> None:
+    st.set_page_config(
+        page_title='ZReader',
+        page_icon='📜',
+        layout='wide',
+        initial_sidebar_state='expanded')
+
+    st.info('Test dashboard')
+
+
+if __name__ == '__main__':
+    main()
