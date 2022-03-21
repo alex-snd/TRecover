7f3907b02d99d56e25e62421de7dcaf8dde9c0e8
alex-snd <alexandershulga.sh@gmail.com>
2022-03-21 19:16:33 +0300

    Test 2 GitHub actions

--- dashboard.py
+++ dashboard.py
@@ -8,7 +8,7 @@ def main() -> None:
         layout='wide',
         initial_sidebar_state='expanded')
 
-    st.info('Test GitHub actions')
+    st.info('Test - 2 GitHub actions')
 
 
 if __name__ == '__main__':
