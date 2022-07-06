5876e4571ac544d143d5df4ed988609f5281f73d
alex-snd <alexandershulga.sh@gmail.com>
2022-07-06 14:14:56 +0300

    Add tune function

--- src/trecover/train/collab/entrypoint.py
+++ src/trecover/train/collab/entrypoint.py
@@ -2,11 +2,19 @@ import time
 from argparse import ArgumentParser
 from typing import List, Optional
 
+import pytorch_lightning as pl
+from pytorch_lightning.utilities import rank_zero_only
+
 from trecover.config import var, exp_var, log
 from trecover.train.collab.arguments import (ModelArguments, TrainingPeerArguments, PLTrainerArguments, DataArguments,
                                              CollaborativeArguments)
+from trecover.train.collab.callback import CollabCheckpoint
 from trecover.train.collab.dht import DHTManager, LocalMetrics
+from trecover.train.collab.strategy import CollaborativeStrategy
+from trecover.train.collab.trainer import LightningWrapper, LightningTuneWrapper
 from trecover.utils.train import parse_dataclasses
+
+rank_zero_only.rank = 1
 
 
 def get_collab_parser() -> ArgumentParser:
@@ -168,53 +176,19 @@ def monitor(args: Optional[List[str]] = None) -> None:
 
 
 def train(args: Optional[List[str]] = None) -> None:
-    from trecover.train.collab.callback import CollabCheckpoint
-    from trecover.train.collab.strategy import CollaborativeStrategy
-    from trecover.train.collab.trainer import LightningWrapper, LightningTuneWrapper
-
-    import pytorch_lightning as pl
-    from pytorch_lightning.utilities import rank_zero_only
-
-    rank_zero_only.rank = 1
-
     data_args, model_args, peer_args, trainer_args, collab_args = parse_dataclasses(DataArguments,
                                                                                     ModelArguments,
                                                                                     TrainingPeerArguments,
                                                                                     PLTrainerArguments,
                                                                                     CollaborativeArguments,
                                                                                     args=args)
-
     if peer_args.initial_peers:
         peer_args.initial_peers = [peer_args.initial_peers, ]
     else:
         peer_args.initial_peers = []
 
     if trainer_args.batch_size is None:
-        log.project_console.print('Trying to find appropriate batch size for this machine', style='magenta')
-
-        if trainer_args.tune_strategy:
-            tune_strategy = CollaborativeStrategy(peer_args, trainer_args, collab_args, tune=True)
-        else:
-            tune_strategy = None
-
-        trainer = pl.Trainer(default_root_dir=exp_var.LIGHTNING_REGISTRY_DIR,
-                             auto_scale_batch_size=True,
-                             auto_select_gpus=True,
-                             accelerator='auto',
-                             strategy=tune_strategy)
-
-        result = trainer.tune(model=LightningTuneWrapper(data_args, model_args, trainer_args),
-                              scale_batch_size_kwargs={
-                                  'init_val': trainer_args.scale_batch_size_init_val,
-                                  'mode': 'binsearch',
-                                  'max_trials': trainer_args.tune_max_trials
-                              })
-        trainer_args.batch_size = result['scale_batch_size']
-        log.project_console.print(f'Found batch size: {trainer_args.batch_size}', style='green')
-
-        if trainer_args.scale_tuned_batch_size:
-            trainer_args.batch_size = int(trainer_args.batch_size // var.BATCH_SIZE_SCALE_FACTOR)
-            log.project_console.print(f'Batch size was scaled to: {trainer_args.batch_size}', style='green')
+        trainer_args.batch_size = tune(args)
 
     dht_manager = DHTManager(peer_args)
     wrapped_model = LightningWrapper(data_args, model_args, trainer_args)
@@ -235,5 +209,43 @@ def train(args: Optional[List[str]] = None) -> None:
     trainer.fit(wrapped_model)
 
 
-if __name__ == '__main__':
-    train()
+def tune(args: Optional[List[str]] = None) -> int:
+    data_args, model_args, peer_args, trainer_args, collab_args = parse_dataclasses(DataArguments,
+                                                                                    ModelArguments,
+                                                                                    TrainingPeerArguments,
+                                                                                    PLTrainerArguments,
+                                                                                    CollaborativeArguments,
+                                                                                    args=args)
+    if peer_args.initial_peers:
+        peer_args.initial_peers = [peer_args.initial_peers, ]
+    else:
+        peer_args.initial_peers = []
+
+    log.project_console.print('Trying to find appropriate batch size for this machine', style='magenta')
+
+    if trainer_args.tune_strategy:
+        tune_strategy = CollaborativeStrategy(peer_args, trainer_args, collab_args, tune=True)
+    else:
+        tune_strategy = None
+
+    trainer = pl.Trainer(default_root_dir=exp_var.LIGHTNING_REGISTRY_DIR,
+                         auto_scale_batch_size=True,
+                         auto_select_gpus=True,
+                         accelerator='auto',
+                         strategy=tune_strategy)
+
+    result = trainer.tune(model=LightningTuneWrapper(data_args, model_args, trainer_args),
+                          scale_batch_size_kwargs={
+                              'init_val': trainer_args.scale_batch_size_init_val,
+                              'mode': 'binsearch',
+                              'max_trials': trainer_args.tune_max_trials
+                          })
+
+    batch_size = result['scale_batch_size']
+    log.project_console.print(f'Found batch size: {batch_size}', style='green')
+
+    if trainer_args.scale_tuned_batch_size:
+        batch_size = int(batch_size // var.BATCH_SIZE_SCALE_FACTOR)
+        log.project_console.print(f'Batch size was scaled to: {trainer_args.batch_size}', style='green')
+
+    return batch_size
