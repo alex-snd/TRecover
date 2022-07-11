eb454637ca84c84183c1ea96f58ce70334ac8d81
alex-snd <alexandershulga.sh@gmail.com>
2022-07-11 17:31:14 +0300

    Restructure

--- src/trecover/train/collab/callback.py
+++ src/trecover/train/collab/callback.py
@@ -23,20 +23,16 @@ class CollabCheckpoint(Callback):
         self.last_reported_collaboration_step = None
         self.min_noise = None
         self.max_noise = None
+        self.lr = None
         self.samples = 0
         self.steps = 0
         self.loss = 0
         self.accuracy = 0
         self.total_samples_processed = 0
+        self.samples_per_second = 0
+        self.alive_peers = 0
         self.backup_every_step = peer_args.backup_every_step
         self.state_path = peer_args.state_path
-
-    def params_are_finite(self):
-        for param in self.pl_module.model.parameters():
-            if not torch.all(torch.isfinite(param)):
-                return False
-
-        return True
 
     def on_train_batch_end(self,
                            trainer: pl.Trainer,
@@ -51,10 +47,10 @@ class CollabCheckpoint(Callback):
 
         if self.pl_module is None:
             self.pl_module = pl_module
-            self.min_noise = pl_module.data_args.min_noise
+            self.min_noise = pl_module.data_args.min_noise  # TODO as property or not, torch.hub.load?
             self.max_noise = pl_module.data_args.max_noise
 
-        if not self.params_are_finite():
+        if not self._params_are_finite():
             log.project_console.print('Model parameters are not finite', style='red')
 
             if not self.state_path.exists():
@@ -69,65 +65,76 @@ class CollabCheckpoint(Callback):
 
         if (current_step := self.optimizer.local_epoch) != self.last_reported_collaboration_step:
             self.total_samples_processed += self.samples
-            samples_per_second = self.optimizer.tracker.performance_ema.samples_per_second
-            current_lr = self.optimizer.opt.param_groups[0]['lr']
+            self.samples_per_second = self.optimizer.tracker.performance_ema.samples_per_second
+            self.lr = self.optimizer.opt.param_groups[0]['lr']
+            self.alive_peers = trainer.strategy.num_peers
 
-            statistics = LocalMetrics(
-                loss=self.loss,
-                accuracy=self.accuracy,
-                lr=current_lr,
-                min_noise=self.min_noise,
-                max_noise=self.max_noise,
-                samples_per_second=samples_per_second,
-                samples_accumulated=self.samples,
-                mini_steps=self.steps,
-                step=current_step - 1
-            )
+            self._report_metrics(trainer, step=current_step)
 
-            panel_group = Group(Text(f'Local loss: {self.loss / self.steps}',
-                                     style='bright_blue', justify='left'),
-                                Text(f'Local accuracy: {self.accuracy / self.steps}',
-                                     style='bright_blue', justify='left'),
-                                Text(f'Learning rate: {current_lr}',
-                                     style='bright_blue', justify='left'),
-                                Text(f'Min-max noise range: {f"{self.min_noise}-{self.max_noise}"}',
-                                     style='bright_blue', justify='left'),
-                                Text(f'Your current contribution: {self.total_samples_processed} samples',
-                                     style='bright_blue', justify='left'),
-                                Text(f'Performance: {samples_per_second} samples/sec',
-                                     style='bright_blue', justify='left'),
-                                Text(f'Peers alive: {trainer.strategy.num_peers}',
-                                     style='bright_blue', justify='left'))
+    def _params_are_finite(self):
+        for param in self.pl_module.model.parameters():
+            if not torch.all(torch.isfinite(param)):
+                return False
 
-            log.project_console.print(
-                Panel(panel_group, title=f'Local step {current_step - 1}',
-                      title_align='left', border_style='magenta'),
-                justify='full'
-            )
+        return True
 
-            self.steps = 0
-            self.loss = 0
-            self.accuracy = 0
+    def _report_metrics(self, trainer: pl.Trainer, step: int) -> None:
+        statistics = LocalMetrics(
+            loss=self.loss,
+            accuracy=self.accuracy,
+            lr=self.lr,
+            min_noise=self.min_noise,
+            max_noise=self.max_noise,
+            samples_per_second=self.samples_per_second,
+            samples_accumulated=self.samples,
+            mini_steps=self.steps,
+            step=step - 1
+        )
 
-            if self.optimizer.local_epoch == self.optimizer.tracker.global_epoch:
-                if not self.dht_manager.dht.store(
-                        key=self.optimizer.run_id + "_metrics",
-                        subkey=self.dht_manager.local_public_key,
-                        value=statistics.dict(),
-                        expiration_time=hivemind.get_dht_time() + self.statistics_expiration,
-                        # return_future=True,
-                ):
-                    log.project_console.print('Failed to store metrics', style='red')
+        self._print_metrics(step=step - 1)
 
-            if not self._should_skip_saving_checkpoint(trainer):
-                log.project_console.print('Backup collab state', style='magenta')
-                trainer.strategy.backup_state()
-            else:
-                log.project_console.print('Skip backup', style='yellow')
+        if self.optimizer.local_epoch == self.optimizer.tracker.global_epoch:
+            if not self.dht_manager.dht.store(
+                    key=self.optimizer.run_id + "_metrics",
+                    subkey=self.dht_manager.local_public_key,
+                    value=statistics.dict(),
+                    expiration_time=hivemind.get_dht_time() + self.statistics_expiration,
+                    return_future=True
+            ):
+                log.project_console.print('Failed to store metrics', style='red')
 
-            self.last_reported_collaboration_step = current_step
+        if not self._should_skip_saving_checkpoint(trainer):
+            log.project_console.print('Backup collab state', style='magenta')
+            trainer.strategy.backup_state()
+        else:
+            log.project_console.print('Skip backup', style='yellow')
 
+        self.steps = 0
+        self.loss = 0
+        self.accuracy = 0
         self.samples = self.optimizer.grad_averager.local_samples_accumulated
+        self.last_reported_collaboration_step = step
+
+    def _print_metrics(self, step: int) -> None:
+        panel_group = Group(Text(f'Local loss: {self.loss / self.steps}',
+                                 style='bright_blue', justify='left'),
+                            Text(f'Local accuracy: {self.accuracy / self.steps}',
+                                 style='bright_blue', justify='left'),
+                            Text(f'Learning rate: {self.lr}',
+                                 style='bright_blue', justify='left'),
+                            Text(f'Min-max noise range: {f"{self.min_noise}-{self.max_noise}"}',
+                                 style='bright_blue', justify='left'),
+                            Text(f'Your current contribution: {self.total_samples_processed} samples',
+                                 style='bright_blue', justify='left'),
+                            Text(f'Performance: {self.samples_per_second} samples/sec',
+                                 style='bright_blue', justify='left'),
+                            Text(f'Peers alive: {self.alive_peers}',
+                                 style='bright_blue', justify='left'))
+
+        log.project_console.print(
+            Panel(panel_group, title=f'Local step {step}', title_align='left', border_style='magenta'),
+            justify='full'
+        )
 
     def _should_skip_saving_checkpoint(self, trainer: pl.Trainer) -> bool:
         from pytorch_lightning.trainer.states import TrainerFn
