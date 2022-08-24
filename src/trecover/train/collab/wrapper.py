f84b95f2e1be3e1e5bd0540df77a40d2410da997
alex-snd <alexandershulga.sh@gmail.com>
2022-08-24 19:04:15 +0300

    Minor changes

--- src/trecover/train/collab/wrapper.py
+++ src/trecover/train/collab/wrapper.py
@@ -92,6 +92,18 @@ class PeerModelWrapper(BaseModelWrapper):
     def __init__(self, args: Namespace, *pl_args: Any, **pl_kwargs: Any):
         super(PeerModelWrapper, self).__init__(args, *pl_args, **pl_kwargs)
 
+    @property
+    def collate(self) -> BaseCollate:
+        from trecover.config import log
+        log.project_console.print('PeerModelWrapper.collate', style='yellow', justify='center')
+        if self._collate is None:
+            if self.args.no_args_sync:
+                self._collate = StandardCollate(min_noise=self.args.min_noise, max_noise=self.args.max_noise)
+            else:
+                self._collate = CollabCollate()
+
+        return self._collate
+
     def training_step(self, batch: Tuple[Tensor, Tensor, Tensor, Optional[Tensor], Optional[Tensor], Tensor],
                       *args, **kwargs
                       ) -> Dict[str, Tensor]:
