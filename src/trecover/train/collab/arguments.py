d38e578495b5049aa85316beae2c5a84b6c0eb53
alex-snd <alexandershulga.sh@gmail.com>
2022-08-30 19:12:36 +0300

    Minor changes

--- src/trecover/train/collab/arguments.py
+++ src/trecover/train/collab/arguments.py
@@ -88,7 +88,7 @@ def get_data_parser(add_help: bool = True) -> ArgumentParser:
                         help='Min noise range')
     parser.add_argument('--max-noise', default=1, type=int,
                         help='Max noise range')
-    parser.add_argument('--n-workers', default=0, type=int,  # TODO investigate
+    parser.add_argument('--n-workers', default=1, type=int,
                         help='Number of processes for dataloaders')
 
     return parser
