a63836c120fae8ec24043a3b1f9d6668c8594a53
alex-snd <alexandershulga.sh@gmail.com>
2022-09-07 17:34:42 +0300

    Minor changes

--- src/trecover/train/collab/arguments.py
+++ src/trecover/train/collab/arguments.py
@@ -140,7 +140,7 @@ def get_optimization_parser(add_help: bool = True) -> ArgumentParser:
                         help='Averaging group will wait for stragglers for at most this many seconds')
     parser.add_argument('--allreduce-timeout', default=80, type=float,
                         help='Give up on a given all-reduce round after this many seconds')
-    parser.add_argument('--averaging-timeout', default=600, type=float,
+    parser.add_argument('--averaging-timeout', default=300, type=float,
                         help='Give up on averaging step after this many seconds')
     parser.add_argument('--no-reuse-grad-buffers', action='store_true',
                         help="Whether or not to use model's grad buffers for accumulating gradients across local steps."
